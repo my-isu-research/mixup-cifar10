7625d99a5b1837116326e75d8eed8320e404d3a3
azibit <abdulazeezidris28@gmail.com>
2022-01-08 08:38:54 -0600

    Update dataset loader to load from given file directory

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+import torchvision
 
 import models
 from utils import progress_bar
@@ -71,14 +72,13 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = datasets.CIFAR10(root='~/data', train=True, download=False,
-                            transform=transform_train)
+trainset = datasets.ImageFolder(os.path.join(dataset, 'train'),
+                                              transform_train)
 trainloader = torch.utils.data.DataLoader(trainset,
                                           batch_size=args.batch_size,
                                           shuffle=True, num_workers=8)
-
-testset = datasets.CIFAR10(root='~/data', train=False, download=False,
-                           transform=transform_test)
+testset = datasets.ImageFolder(os.path.join(dataset, 'test'),
+                                              transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=False, num_workers=8)
 
