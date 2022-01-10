d1ca0e8c2bc647dd662e2dc64334d9d0e34b0320
azibit <abdulazeezidris28@gmail.com>
2022-01-10 13:51:14 -0600

    Update transform for test data

--- train.py
+++ train.py
@@ -179,6 +179,7 @@ if args.augment:
     ])
 else:
     transform_train = transforms.Compose([
+        transforms.RandomCrop(224, padding=4),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465),
                              (0.2023, 0.1994, 0.2010)),
@@ -186,6 +187,7 @@ else:
 
 
 transform_test = transforms.Compose([
+    transforms.RandomCrop(224, padding=4),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
